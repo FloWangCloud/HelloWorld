cee5663aa9e734e41919417ee8e10624dd5f25a7
FloWangCloud <77180923+FloWangCloud@users.noreply.github.com>
2021-01-08 18:41:42 -0800

    Add files via upload

---
+++ YiranWangProject1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YiranWangProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Yiran Wang!");
+        }
+    }
+}
